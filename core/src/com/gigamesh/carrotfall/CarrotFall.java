9500ba10fd25fc217de1ce54af04756092455b26
jermaine <jaylarcane@gmail.com>
2017-01-06 14:24:57 -0500

    declared members for physics and ecs

--- core/src/com/gigamesh/carrotfall/CarrotFall.java
+++ core/src/com/gigamesh/carrotfall/CarrotFall.java
@@ -1,33 +1,55 @@
 package com.gigamesh.carrotfall;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
-public class CarrotFall extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class CarrotFall extends Game {
+    SpriteBatch batch;
+    Texture img;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    /* view settings */
+    OrthographicCamera camera;
+    FitViewport viewport;
+
+    /* UI */
+    Stage stage;
+    Skin skin;
+
+    /* Box2D */
+    World world;
+    Box2DDebugRenderer box2DDebugRenderer;
+
+    /* Ashley ECS */
+    Engine ashleyEngine;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 }
