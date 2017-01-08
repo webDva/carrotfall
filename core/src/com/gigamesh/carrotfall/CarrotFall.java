6baa09b8fa1a49ef0be437b5d674bffe2852ac38
jermaine <jaylarcane@gmail.com>
2017-01-08 10:15:59 -0500

    started to fill in the physics component

--- core/src/com/gigamesh/carrotfall/CarrotFall.java
+++ core/src/com/gigamesh/carrotfall/CarrotFall.java
@@ -1,6 +1,7 @@
 package com.gigamesh.carrotfall;
 
 import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
@@ -9,6 +10,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2D;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
@@ -19,6 +21,8 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+import ecs.components.PhysicsComponent;
 
 public class CarrotFall extends Game {
     private SpriteBatch batch;
@@ -38,6 +42,7 @@ public class CarrotFall extends Game {
 
     /* Ashley ECS */
     private Engine ashleyEngine;
+    private Entity carrotEntity;
 
     @Override
     public void create() {
@@ -56,6 +61,13 @@ public class CarrotFall extends Game {
 
         /* ashley ecs */
         ashleyEngine = new Engine(); // a blank engine for now
+        carrotEntity = new Entity();
+
+        carrotEntity.add(new PhysicsComponent());
+
+        /* filling the physics body */
+        PhysicsComponent physicsComponent = carrotEntity.getComponent(PhysicsComponent.class);
+        physicsComponent.bodyDef = new BodyDef();
 
         /* initialize ui */
         ui_stage = new Stage(new ScreenViewport(), batch);
