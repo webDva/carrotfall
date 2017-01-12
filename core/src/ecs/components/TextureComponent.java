edab10630dfd2b7050ea164debaa5116208e4237
jermaine <jaylarcane@gmail.com>
2017-01-12 16:10:54 -0500

    added carrot.png texture!

--- core/src/ecs/components/TextureComponent.java
+++ core/src/ecs/components/TextureComponent.java
@@ -1,6 +1,7 @@
 package ecs.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 
 /**
@@ -9,4 +10,8 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class TextureComponent implements Component {
     public Texture texture;
+
+    public TextureComponent(String path) {
+        this.texture = new Texture(Gdx.files.internal(path)); // constructor's here, because don't wanna load texture elsewhere
+    }
 }
